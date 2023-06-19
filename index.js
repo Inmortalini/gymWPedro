05a3f03d06f078419616a0f09a1728261ab06447
inmortalini <pedrobarrientos@gmail.com>
2023-06-19 09:44:21 -0400

    Agregando imagenes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function toggleImageVisibility(element) {
-    var img = element.nextElementSibling;
-    img.style.display = img.style.display === 'none' ? 'block' : 'none';
-  }
+  var img = element.nextElementSibling;
+  img.style.display = img.style.display === 'none' ? 'block' : 'none';
+
+  // Cerrar la imagen después de 5 segundos
+  setTimeout(function() {
+    img.style.display = 'none';
+  }, 10000); // 15000 milisegundos = 15 segundos
+}
